50d6b196505388dd989deeee99bcadf17563d82e
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-19 02:54:12 +0600

    Update

--- src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
+++ src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
-import { Dropdown, DropdownToggle } from "reactstrap";
+import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from "reactstrap";
 
 //i18n
-import { withTranslation } from "react-i18next";
+import {withTranslation} from "react-i18next";
 // Redux
-import { withRouter } from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 
 // users
 import user1 from "../../../assets/images/users/avatar-1.jpg";
@@ -22,8 +22,8 @@ const ProfileMenu = props => {
         const obj = JSON.parse(localStorage.getItem("authUser"));
         setusername(obj.displayName);
       } else if (
-        process.env.REACT_APP_DEFAULTAUTH === "fake" ||
-        process.env.REACT_APP_DEFAULTAUTH === "jwt"
+          process.env.REACT_APP_DEFAULTAUTH === "fake" ||
+          process.env.REACT_APP_DEFAULTAUTH === "jwt"
       ) {
         const obj = JSON.parse(localStorage.getItem("authUser"));
         setusername(obj.username);
@@ -32,52 +32,52 @@ const ProfileMenu = props => {
   }, [props.success]);
 
   return (
-    <React.Fragment>
-      <Dropdown
-        isOpen={menu}
-        toggle={() => setMenu(!menu)}
-        className="d-inline-block"
-      >
-        <DropdownToggle
-          className="btn header-item "
-          id="page-header-user-dropdown"
-          tag="button"
+      <React.Fragment>
+        <Dropdown
+            isOpen={menu}
+            toggle={() => setMenu(!menu)}
+            className="d-inline-block"
         >
-          <img
-            className="rounded-circle header-profile-user"
-            src={user1}
-            alt="Header Avatar"
-          />
-          <span className="d-none d-xl-inline-block ms-2 me-1">{username}</span>
-          {/*<i className="mdi mdi-chevron-down d-none d-xl-inline-block"/>*/}
-        </DropdownToggle>
-        {/*<DropdownMenu className="dropdown-menu-end">*/}
-        {/*  <DropdownItem tag="a" href="/profile">*/}
-        {/*    {" "}*/}
-        {/*    <i className="bx bx-user font-size-16 align-middle me-1"/>*/}
-        {/*    {props.t("Profile")}{" "}*/}
-        {/*  </DropdownItem>*/}
-        {/*  <DropdownItem tag="a" href="/crypto-wallet">*/}
-        {/*    <i className="bx bx-wallet font-size-16 align-middle me-1"/>*/}
-        {/*    {props.t("My Wallet")}*/}
-        {/*  </DropdownItem>*/}
-        {/*  <DropdownItem tag="a" href="#">*/}
-        {/*    <span className="badge bg-success float-end">11</span>*/}
-        {/*    <i className="bx bx-wrench font-size-16 align-middle me-1"/>*/}
-        {/*    {props.t("Settings")}*/}
-        {/*  </DropdownItem>*/}
-        {/*  <DropdownItem tag="a" href="auth-lock-screen">*/}
-        {/*    <i className="bx bx-lock-open font-size-16 align-middle me-1"/>*/}
-        {/*    {props.t("Lock screen")}*/}
-        {/*  </DropdownItem>*/}
-        {/*  <div className="dropdown-divider"/>*/}
-        {/*  <Link to="/logout" className="dropdown-item">*/}
-        {/*    <i className="bx bx-power-off font-size-16 align-middle me-1 text-danger"/>*/}
-        {/*    <span>{props.t("Logout")}</span>*/}
-        {/*  </Link>*/}
-        {/*</DropdownMenu>*/}
-      </Dropdown>
-    </React.Fragment>
+          <DropdownToggle
+              className="btn header-item "
+              id="page-header-user-dropdown"
+              tag="button"
+          >
+            <img
+                className="rounded-circle header-profile-user"
+                src={user1}
+                alt="Header Avatar"
+            />
+            <span className="d-none d-xl-inline-block ms-2 me-1">{username}</span>
+            {/*<i className="mdi mdi-chevron-down d-none d-xl-inline-block"/>*/}
+          </DropdownToggle>
+          <DropdownMenu className="dropdown-menu-end">
+            <DropdownItem tag="a" href="/profile">
+              {" "}
+              <i className="bx bx-user font-size-16 align-middle me-1"/>
+              {props.t("Profile")}{" "}
+            </DropdownItem>
+            <DropdownItem tag="a" href="/crypto-wallet">
+              <i className="bx bx-wallet font-size-16 align-middle me-1"/>
+              {props.t("My Wallet")}
+            </DropdownItem>
+            <DropdownItem tag="a" href="#">
+              <span className="badge bg-success float-end">11</span>
+              <i className="bx bx-wrench font-size-16 align-middle me-1"/>
+              {props.t("Settings")}
+            </DropdownItem>
+            <DropdownItem tag="a" href="auth-lock-screen">
+              <i className="bx bx-lock-open font-size-16 align-middle me-1"/>
+              {props.t("Lock screen")}
+            </DropdownItem>
+            <div className="dropdown-divider"/>
+            <Link to="/logout" className="dropdown-item">
+              <i className="bx bx-power-off font-size-16 align-middle me-1 text-danger"/>
+              <span>{props.t("Logout")}</span>
+            </Link>
+          </DropdownMenu>
+        </Dropdown>
+      </React.Fragment>
   );
 };
 
